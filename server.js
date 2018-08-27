e8ef6ab493ca528fd1ca494f45cf35fd248c0d10
Loser Teach <jetaimefrc@gmail.com>
2018-08-27 22:12:56 +0700

    Comple HBS and use middleWare

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-
+const fs = require('fs');
 app.set('view engine', 'hbs');
 
 hbs.registerPartials(__dirname + '/views/partials');
 /**
  * Dùng để hiển thị trang html || cái # trong folder #
  */
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}\n${req.method}\nlocallhost:3000${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next(); // middleware
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
 app.use(express.static(__dirname + '/public'));
+ 
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
